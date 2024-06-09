5990c5c08af6e4596f0342d85d698a3c73f25036
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-06-09 21:50:27 +0300

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -6,12 +6,18 @@ def call(String OpenShiftCredentialsID, String openshiftClusterurl, String opens
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
+     withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'kubernetes', namespace: '', serverUrl: '']]) 
+			sh"""
+                kubectl apply -f .
+            """
+			}
+
     // login to OpenShift Cluster via cluster url & service account token
-    withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
-            sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-            sh "oc project ${openshiftProject}"
-            sh "oc apply -f ."
-    }
+    // withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
+    //         sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
+    //         sh "oc project ${openshiftProject}"
+    //         sh "oc apply -f ."
+    // }
 
     // login to OpenShift Cluster via KubeConfig file
     //withCredentials([file(credentialsId: 'OpenShiftCredentialsID', variable: 'KUBECONFIG_FILE')]) {
