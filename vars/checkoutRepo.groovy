c39372a1f97b48d1a35f4239dc0f36127e23f9d5
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-06-09 21:55:31 +0300

    Update checkoutRepo.groovy

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
 def call() {
 	echo "checking Git Repo..."
-	checkout scm	
+	checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'GitHub', url: 'https://github.com/Osamaomera/MultiCloudDevOpsProject.git']])	
 }
