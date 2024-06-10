cd02a365db08ae90b3c80d38248b88c000dcb441
Osamaomera <osama.omera68@gmail.com>
2024-06-10 20:25:24 +0300

    sonar

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -1,10 +1,8 @@
 #!usr/bin/env groovy
 def call(){ 
-	
-	echo "Running SonarQube Test"
-	// withSonarQubeEnv() { 
-    //     	echo "Running SonarQube Analysis..."
-	// 		sh 'chmod +x gradlew'
-    //     	sh "./gradlew sonar" 
-	// }
+	withSonarQubeEnv() { 
+        	echo "Running SonarQube Analysis..."
+			sh 'chmod +x gradlew'
+        	sh "./gradlew sonar" 
+	}
 }
