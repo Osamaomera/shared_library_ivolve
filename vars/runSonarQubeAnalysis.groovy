b0d7038723b8a5fed5ced13a8ac678b474dee324
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-06-11 03:55:16 +0300

    Update runSonarQubeAnalysis.groovy

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -1,10 +1,9 @@
 #!usr/bin/env groovy
 def call(){ 
-	
 	echo "Running SonarQube "
-	// withSonarQubeEnv() { 
-    //     	echo "Running SonarQube Analysis..."
-	// 		sh 'chmod +x gradlew'
-    //     	sh "./gradlew sonar" 
-	// }
+	withSonarQubeEnv(credentialsId: 'SonarQube') {
+		echo "Running SonarQube Analysis..."
+		sh 'chmod +x gradlew'
+		sh "./gradlew sonar"
+	}
 }
