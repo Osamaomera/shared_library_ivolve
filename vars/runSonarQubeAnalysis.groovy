81cfd255a4d42111d6f019d4a46ce8ea01e68e82
Osamaomera <osama.omera68@gmail.com>
2024-06-11 04:14:49 +0300

    sonar

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -3,6 +3,9 @@ def call(){
 	echo "Running SonarQube "
 	withSonarQubeEnv(credentialsId: 'SonarQube') {
 		echo "Running SonarQube Analysis..."
+		sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Devops-CICD \
+		-Dsonar.java.binaries=. \
+		-Dsonar.projectKey=Devops-CICD '''
 		sh 'chmod +x gradlew'
 		sh "./gradlew sonar"
 	}
