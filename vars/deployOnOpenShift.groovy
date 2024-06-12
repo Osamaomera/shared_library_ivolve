759d031e99d8cfca4aa21a33e0f2052515ca3be0
Osama Omera <92927320+Osamaomera@users.noreply.github.com>
2024-06-12 17:07:51 +0300

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -4,7 +4,7 @@
 def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
     
     // Update deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
+    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
 
     // login to OpenShift Cluster via cluster url & service account token
     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
